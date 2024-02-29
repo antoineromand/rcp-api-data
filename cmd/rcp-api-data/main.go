0c58919b61e2ac525ed1337de3d834e5499c6777
Antoine Romand <antoine.romand@epitech.eu>
2024-02-29 17:22:41 +0100

    load env file and use for http server port

--- cmd/rcp-api-data/main.go
+++ cmd/rcp-api-data/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	Config "rcp-api-data/internal/config"
+
+	"github.com/caarlos0/env/v10"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	err := http.ListenAndServe(":3333", nil)
+	loadEnv := godotenv.Load()
+	if loadEnv != nil {
+		fmt.Print(loadEnv)
+	}
+	cfg := Config.Environment{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+	fmt.Println("Port:", cfg.Port)
+	err := http.ListenAndServe(":" + cfg.Port, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
