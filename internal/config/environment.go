2f0ce2b6b0c6164a9e2163975a7860f873f80578
Antoine Romand <antoine.romand@epitech.eu>
2024-03-01 11:27:52 +0100

    create environment file and use init to load env and parse

--- internal/config/environment.go
+++ internal/config/environment.go
@@ -1,4 +1,36 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env/v10"
+	"github.com/joho/godotenv"
+)
 type Environment struct {
 	Port string `env:"PORT"`
+	RCP_AUTH_URL string `env:"RCP_AUTH_URL"`
+	RCP_AUTH_PORT string `env:"RCP_AUTH_PORT"`
+	RCP_AUTH_PROTOCOL string `env:"RCP_AUTH_PROTOCOL"`
 }
+
+var cfg Environment
+
+func init() {
+	loadEnv := godotenv.Load()
+	if loadEnv != nil {
+		fmt.Print(loadEnv)
+	}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Erreur lors du parsing des variables d'environnement : %+v\n", err)
+	}
+}
+
+func (e *Environment) GetAuthURL() string {
+	return e.RCP_AUTH_PROTOCOL + "://" + e.RCP_AUTH_URL + ":" + e.RCP_AUTH_PORT
+}
+
+func GetConfig() *Environment {
+	return &cfg
+}
+
