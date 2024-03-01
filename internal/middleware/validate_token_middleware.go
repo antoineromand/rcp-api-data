f0adaad26d83abd553869c08b54f50639f28be60
Antoine Romand <antoine.romand@epitech.eu>
2024-03-01 11:29:46 +0100

    create middleware token and import jwt config file (decode method)

--- internal/middleware/validate_token_middleware.go
+++ internal/middleware/validate_token_middleware.go
@@ -2,56 +2,48 @@ package middleware
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"net/http"
-	"strings"
+	"rcp-api-data/internal/config"
 )
 
-type TokenFromCookie struct {
-    UUID string
-}
 type CtxTokenKey string
 
-const tokenKey CtxTokenKey = "token"
+const TokenKey CtxTokenKey = "token"
 
 func ValidateTokenMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         // Envoi de la requête GET à l'API d'authentification
-        resp, err := http.Get("URL_DE_VOTRE_API/validateToken")
+        cfg := config.GetConfig()
+        if cfg == nil { 
+            http.Error(w, "Erreur lors de la requête à l'API d'authentification 1", http.StatusInternalServerError)
+            return 
+        }
+        resp, err := http.Get(cfg.GetAuthURL() + "/token-check")
         if err != nil {
-            http.Error(w, "Erreur lors de la requête à l'API d'authentification", http.StatusInternalServerError)
+            http.Error(w, "Erreur lors de la requête à l'API d'authentification 2", http.StatusInternalServerError)
             return
         }
         defer resp.Body.Close()
-
         // Vérifiez si le statut de la réponse est 200
         if resp.StatusCode == http.StatusOK {
             // Extraire le token JWT du cookie
-            cookie, err := r.Cookie("jwt")
+            //cookie, err := r.Cookie("token")
+            //fmt.Print(cookie.Value)
+            cookie := "eyJ1dWlkIjoiYTU0MjUxZGMtODdhNi00YTkzLWFlYTYtNzU5YTIwYzBhZDZkIiwiZXhwIjoiMjAyNC0wMy0wMlQwOToxOToyNy4yNzVaIiwiaXNzdWVyIjoiYXV0aGVudGljYXRpb24tMS42LjEiLCJhdWRpZW5jZSI6ImFuZGVzaXRlIn0=.eyJ1dWlkIjoiOTA1ZTkwNWQtNDZkMy00NjE3LWFkNzctYzJiODNhNDMzODRhIiwidXNlcm5hbWUiOiJhbmRlc2l0ZSIsInJvbGVQZXJtaXNzaW9uIjp7ImFkbWluIjpbImFkbWluIl19fQ==.jk/hiegvenlvIFitfCyX+6C1TIFDOD0MczNq1vT+bneSHmdk6Yj33j3xeAC1a3LMO8z4E4mAp9E+m6mu9KzRDg=="
             if err != nil {
                 http.Error(w, "Token JWT introuvable dans le cookie", http.StatusUnauthorized)
                 return
             }
 
-            // Décoder le payload du token
-            splitToken := strings.Split(cookie.Value, ".")
-            if len(splitToken) != 3 {
-                http.Error(w, "Token JWT invalide", http.StatusUnauthorized)
-                return
-            }
+            // Décoder le token JWT
+            token, err := config.DecodePayload(cookie)
 
-            payload, err := base64.RawURLEncoding.DecodeString(splitToken[1])
             if err != nil {
                 http.Error(w, "Erreur lors du décodage du token JWT", http.StatusUnauthorized)
                 return
             }
-
-            var token TokenFromCookie
-            json.Unmarshal(payload, &token)
-
             // Ajouter les informations décodées à la requête, si nécessaire
-            newRequest := r.WithContext(context.WithValue(r.Context(), tokenKey, token))
+            newRequest := r.WithContext(context.WithValue(r.Context(), TokenKey, token))
 
             // Passez à la prochaine manipulation dans la chaîne
             next.ServeHTTP(w, newRequest)
