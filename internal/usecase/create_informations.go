2176034704ec1974495fb298680e7db95443e173
Antoine Romand <antoine.romand@epitech.eu>
2024-03-28 14:30:23 +0100

    update get informations usecase (create if not) + delete create informations usecase and post implementation in controller

--- internal/usecase/create_informations.go
+++ internal/usecase/create_informations.go
@@ -1,53 +1,53 @@
 package usecase
 
-import (
-	"rcp-api-data/internal/common"
-	"rcp-api-data/internal/mapper"
-	"rcp-api-data/internal/repository"
-	"rcp-api-data/internal/utils"
+// import (
+// 	"rcp-api-data/internal/common"
+// 	"rcp-api-data/internal/mapper"
+// 	"rcp-api-data/internal/repository"
+// 	"rcp-api-data/internal/utils"
 
-	"gorm.io/gorm"
-)
+// 	"gorm.io/gorm"
+// )
 
-func CreateInformations(db *gorm.DB, uuid string, account []byte) *common.Response {
-	sugar := utils.GetLogger()
-	accountRepository := repository.AccountRepository{DB: db}
-	convertedUUID, err := utils.ConvertStringToUUID(uuid)
-	if err != nil {
-		sugar.Error("Error while converting string to UUID", err)
-		return &common.Response{
-			Data: nil,
-			Error: &common.CustomError{
-				Message: "Error while converting string to UUID",
-			},
-			Code: 400,
-		}
-	}
-	accountEntity, err := mapper.AccountMapping(account, &convertedUUID)
-	if err != nil {
-		sugar.Error("Error while mapping account", err)
-		return &common.Response{
-			Data: nil,
-			Error: &common.CustomError{
-				Message: "Error while mapping account",
-			},
-			Code: 400,
-		}
-	}
-	err = accountRepository.CreateAccount(&accountEntity)
-	if err != nil {
-		sugar.Error("Error while creating account profile", err)
-		return &common.Response{
-			Data: nil,
-			Error: &common.CustomError{
-				Message: "Error while creating account profile",
-			},
-			Code: 400,
-		}
-	}
-	return &common.Response{
-		Data: map[string]string{"message": "Account Profile created successfully"},
-		Error: nil,
-		Code: 201,
-	}
-}
+// func CreateInformations(db *gorm.DB, uuid string, username string, account []byte) *common.Response {
+// 	sugar := utils.GetLogger()
+// 	accountRepository := repository.AccountRepository{DB: db}
+// 	convertedUUID, err := utils.ConvertStringToUUID(uuid)
+// 	if err != nil {
+// 		sugar.Error("Error while converting string to UUID", err)
+// 		return &common.Response{
+// 			Data: nil,
+// 			Error: &common.CustomError{
+// 				Message: "Error while converting string to UUID",
+// 			},
+// 			Code: 400,
+// 		}
+// 	}
+// 	accountEntity, err := mapper.AccountMapping(account, &convertedUUID, username)
+// 	if err != nil {
+// 		sugar.Error("Error while mapping account", err)
+// 		return &common.Response{
+// 			Data: nil,
+// 			Error: &common.CustomError{
+// 				Message: "Error while mapping account",
+// 			},
+// 			Code: 400,
+// 		}
+// 	}
+// 	err = accountRepository.CreateAccount(&accountEntity)
+// 	if err != nil {
+// 		sugar.Error("Error while creating account profile", err)
+// 		return &common.Response{
+// 			Data: nil,
+// 			Error: &common.CustomError{
+// 				Message: "Error while creating account profile",
+// 			},
+// 			Code: 400,
+// 		}
+// 	}
+// 	return &common.Response{
+// 		Data:  map[string]string{"message": "Account Profile created successfully"},
+// 		Error: nil,
+// 		Code:  201,
+// 	}
+// }
--- internal/usecase/get_informations.go
+++ internal/usecase/get_informations.go
@@ -2,22 +2,55 @@ package usecase
 
 import (
 	"rcp-api-data/internal/common"
+	account_entity "rcp-api-data/internal/entity/domain/account"
 	"rcp-api-data/internal/repository"
 	"rcp-api-data/internal/utils"
 
 	"gorm.io/gorm"
 )
 
-
-func GetInformationsByUserUuid(db *gorm.DB, uuid string) common.Response {
+func GetInformationsByUserUuid(db *gorm.DB, uuid string, username string) common.Response {
 	sugar := utils.GetLogger()
 	accountRepository := repository.AccountRepository{DB: db}
 	account, err := accountRepository.GetAccountByUserUUID(uuid)
+	if err == gorm.ErrRecordNotFound {
+		convertedUUID, err := utils.ConvertStringToUUID(uuid)
+		if err != nil {
+			sugar.Error("Error while converting string to UUID", err)
+			return common.Response{
+				Code: 400,
+				Error: &common.CustomError{
+					Message: "Error while converting string to UUID",
+				},
+				Data: nil,
+			}
+		}
+		dto := &account_entity.Account{
+			UserUUID: convertedUUID,
+			Username: &username,
+		}
+		account, err := accountRepository.CreateAccount(dto)
+		if err != nil {
+			sugar.Error("Error while creating account profile", err)
+			return common.Response{
+				Code: 400,
+				Error: &common.CustomError{
+					Message: "Error while creating account profile",
+				},
+				Data: nil,
+			}
+		}
+		return common.Response{Code: 200, Error: nil, Data: account}
+	}
 	if err != nil {
-		sugar.Error("Error while getting account by user uuid, account not found", err)
-		return common.Response{Code: 500, Error: &common.CustomError{
-			Message: "Error while getting account by user uuid, account not found",
-		}, Data: nil}
+		sugar.Error("Error while getting account profile", err)
+		return common.Response{
+			Code: 400,
+			Error: &common.CustomError{
+				Message: "Error while getting account profile",
+			},
+			Data: nil,
+		}
 	}
 	return common.Response{Code: 200, Error: nil, Data: account}
 }
